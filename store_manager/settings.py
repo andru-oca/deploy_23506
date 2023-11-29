136f32ffec8281ce9504041eb989453263cf9d98
andru-oca <112781394+andru-oca@users.noreply.github.com>
2023-11-29 00:54:15 +0000

    collect static

--- store_manager/settings.py
+++ store_manager/settings.py
@@ -142,9 +142,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "staticfiles"
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles"
+]
 
 
 STATIC_ROOT = BASE_DIR / "static"
